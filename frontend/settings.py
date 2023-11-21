15cf1c448c6a4c4f455c850ed197aa2e9f8df66d
Vladimir Dimitrov <vladimir.dimitrov@astrazeneca.com>
2023-11-21 14:42:24 +0200

    fix init bluetooth page

--- frontend/settings.py
+++ frontend/settings.py
@@ -3,13 +3,15 @@ import bluetooth
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 
-def tupleToObject(t):
-    return {"addr": t[0], "name": t[1]}
-
-
 def findBluetoothDevices():
     devices = (bluetooth.discover_devices(lookup_names=True),)
-    return map(tupleToObject, devices)
+    # print(devices)
+    listDevices = []
+    index = 0
+    for device in devices:
+        listDevices[index] = {"addr": device[0], "name": device[1]}
+        index = index + 1
+    return listDevices
 
 
 def findAvailablePort():
