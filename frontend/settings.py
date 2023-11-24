a87cfecc4800a55c7f98df7a7ab78b7e8a509735
Vladimir Dimitrov <vladimir.dimitrov@astrazeneca.com>
2023-11-24 16:28:49 +0200

    now that scanning for devices work, trying to connect to new device(not trusted)

--- frontend/settings.py
+++ frontend/settings.py
@@ -18,19 +18,10 @@ def find_bluetooth_devices():
 
 
 def connect_to_bt_device(device):
-    # device_address = device["addr"]
+    device_address = device["addr"]
     print(f"device: {device}")
 
-    # pair_and_connect(device_address, 1)
-
-    target_name = device["name"]  # Replace with the name of your target device
-    target_address = find_device_address(target_name)
-
-    if target_address:
-        print(f"Device '{target_name}' found at address {target_address}.")
-        pair_and_connect(target_address)
-    else:
-        print(f"Device with name '{target_name}' not found.")
+    pair_and_connect(device_address, 1)
 
     print("Connected to: " + device["name"])
     return True
@@ -42,31 +33,6 @@ def run_command(command):
     )
     output, error = process.communicate()
     return output.decode(), error.decode()
-
-
-def find_device_address(target_device_name):
-    # Start Bluetooth scanning
-    run_command("sudo hcitool scan > /tmp/bt_scan_results &")
-
-    # Wait for the scan to complete
-    time.sleep(10)
-
-    # Read the scan results
-    output, _ = run_command("cat /tmp/bt_scan_results")
-    print(f"output {output}")
-
-    # Check if the desired device is in the scan results
-    target_device_address = None
-    for line in output.split("\n"):
-        print(f"{line}")
-        if target_device_name in line:
-            target_device_address = line.split()[0]
-            break
-
-    # Clean up temporary files
-    run_command("rm /tmp/bt_scan_results")
-
-    return target_device_address
 
 
 def pair_and_connect(device_address):
